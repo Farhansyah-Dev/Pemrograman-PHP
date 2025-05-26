ee351892b7e7364814633c5bf36c630bc32d85f5
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-05-26 18:01:08 +0700

    membuat folder tipe data dan membuat file baru

---
+++ Tipe Data/TipeData.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Tipe Daata PHP\n";
+// Tipe data integer
+
+echo "Tipe data integer";
+echo "Decimal: ";
+var_dump(1234); // tipe data integer dengan notasi desimal
+
+echo "Oktal :";
+var_dump(0123); // tipe data integer dengan notasi Oktal
+
+echo "Hexadecimal :";
+var_dump(0x1A); // tipe data integer dengan notasi HexaDecimal
+
+echo "Binary :";
+var_dump(0b11111111); // tipe data integer dengan notasi Binary
+
+echo "Underscore di number :";
+var_dump(8_45000000);
+
+
