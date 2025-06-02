6e16576f9f5c7bde9c0ad69f6feeeebaafd5c0d9
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-06-02 22:28:25 +0700

    menambahkan folder dan file variabel

---
+++ Variabel/variabel.php
@@ -0,0 +1,15 @@
+<!-- contoh Variable pada PHP -->
+<?php
+// Deklarasi variabel
+
+$merk_laptop = "Huawei"; // variabel dengan nama $merk_laptop dan nilai "Huawei"
+$harga_laptop = 9_000000; // variabel dengan nama $harga_laptop dan nilai 9.000.000
+$ukuran_layar = 14.0; // variabel dengan nama $ukuran_layar dan nilai 14.0
+$warna_laptop = "Silver"; // variabel dengan nama $warna_laptop dan nilai "Silver"
+
+echo "Merk Laptop\t: $merk_laptop \n";
+echo "Harga Laptop Rp\t: $harga_laptop \n";
+echo "Ukuran Layar\t: $ukuran_layar \n";
+echo "Warna Laptop\t: $warna_laptop \n";
+
+var_dump($harga_laptop); // menampilkan tipe data dari variabel $harga_laptop
