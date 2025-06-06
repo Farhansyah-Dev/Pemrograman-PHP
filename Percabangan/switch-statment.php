d37c1e0cdad55ea81b26327da67989b4353f8d61
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-06-06 19:51:55 +0700

    modify ifelse and file switch

---
+++ Percabangan/switch-statment.php
@@ -0,0 +1,19 @@
+<?php
+// switch statement
+$nilai = "G";
+
+switch ($nilai) {
+    case "A":
+        echo "Anda Lulus dengan sangat baik" . PHP_EOL;
+        break;
+    case "B":
+    case "C":
+        echo "Anda Lulus" . PHP_EOL;
+        break;
+    case "D";
+        echo "Anda Tidak Lulus" . PHP_EOL;
+        break;
+    default:
+    echo "Mungkin Anda Salah Jurusan" . PHP_EOL;
+        break;
+}
