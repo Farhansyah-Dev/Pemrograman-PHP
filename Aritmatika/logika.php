2f5eec0d3ba11a39bd9d76f7ae81ae030867b701
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-06-06 09:30:45 +0700

    created file logika

---
+++ Aritmatika/logika.php
@@ -0,0 +1,16 @@
+<?php
+// opetator logika
+// operator logika digunakan untuk menggabungkan beberapa kondisi
+var_dump(true && true); // AND (kondisi benar jika kedua atau salah satau kondisi benar)
+var_dump(true && false); 
+var_dump(false && false); 
+// bisa menggunakan simbol && atau and untuk melambangkan AND
+var_dump(true || true); // OR (Kondisi benar jika salah satu atau kedua kondisi benar)
+var_dumP(true || false);
+var_dump(false || false);
+// bisa menggunakan simbol || atau or untuk melambangkan OR
+var_dump(!true); //NOT (kondisi salah jika kondisi benar)
+var_dump(!false); // kondisi benar jika kondisi salah
+// bisa menggunakan simbol ! atau not untuk melambangkan NOT
+var_dump(true xor true); // XOR (kondisi benar jika salah satu kondisi benar, tapi tidak kedua-duanya)
+var_dump(true xor true);
