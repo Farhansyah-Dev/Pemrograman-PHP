f92f6e4e2e959bf01e803e653a94f20d4fce0e90
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-06-06 08:02:09 +0700

    add aritmatika

---
+++ Aritmatika/aritmatika.php
@@ -0,0 +1,19 @@
+// operator aritmatika
+// penjumlahan, pengurangan, perkalian, pembagian, modulus
+<?php
+
+$a = 10; // inisialisasi variabel a dengan nilai 10
+$b = 10; // inisialisasi variabel b dengan nilai 10
+
+$tambah = $a + $b; // penjumlahan
+$kurang = $a - $b; // pengurangan
+$kali = $a * $b; // perkalian
+$bagi = $a / $b; // pembagian
+$modulus = $a % $b; // modulus sisa bagi
+$pangkat = $a ** $b; // pangkat
+echo "Hasil penjumlahan $a + $b = $tambah\n"; // menampilkan hasil penjumlahan
+echo "Hasil penjumlahan $a - $b = $kurang\n"; // menampilkan hasil pengurangan
+echo "Hasil penjumlahan $a * $b = $kali\n"; // menampilkan hasil perkalian
+echo "Hasil penjumlahan $a / $b = $bagi\n"; // menampilkan hasil pembagian
+echo "Hasil penjumlahan $a % $b = $modulus\n"; // menampilkan hasil modulus
+echo "Hasil penjumlahan $a ** $b = $pangkat\n"; // menampilkan hasil pangkat
