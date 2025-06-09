63cb9cd803d2a7f0bc0710e8ac1b5ffaa655e9e0
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-06-09 10:34:37 +0700

    menambahkan argument tipe data

--- Function/Function.php
+++ Function/Function.php
@@ -12,3 +12,21 @@ function CallName($name) {
 }
 CallName('Ardiansyah');
 CallName('Farhan Mubarok');
+
+// Default Argument
+function NameArgument ($name, $age = 30) {
+    echo "Hi, There My Name: $name" . PHP_EOL;
+    echo "My Age: $age" . PHP_EOL;
+}
+NameArgument('Riski');
+NameArgument('Dandi', 21);
+
+// Argument Tipe data
+function sum(int $a, int $b) {
+    $total = $a + $b ;
+    echo "Total = $a + $b = $total" . PHP_EOL;
+}
+sum('100','100');
+sum(100, 100);
+sum(true, true);
+
