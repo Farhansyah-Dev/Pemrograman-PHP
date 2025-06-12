f0c48434baa6671071d7b78b6a1f818149682bed
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-06-12 18:03:56 +0700

    Membuat String Function

---
+++ StringFunction/StrFunction.php
@@ -0,0 +1,9 @@
+<?php
+var_dump (join(" ", [1, 2, 3, 4, 5]));
+var_dump (explode(' ', 'Muhammad Farhan'));
+var_dump (strtolower('MUHAMMAD FARHAN'));
+var_dump (strtoupper('muhammad farhan'));
+var_dump (trim('    muhammad     '));
+var_dump (substr('Muhammad Farhan', 0, 6));
+var_dump (addcslashes('Farhan[,]', 'a..z'));
+var_dump (addslashes('Apa Kabar mu Teman'));
