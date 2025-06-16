c1354c95d514a9c2b217077600c7e290e475b50d
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-06-16 20:41:52 +0700

    update file

--- Variabel/variabel.php
+++ Variabel/variabel.php
@@ -7,9 +7,9 @@ $harga_laptop = 9_000000; // variabel dengan nama $harga_laptop dan nilai 9.000.
 $ukuran_layar = 14.0; // variabel dengan nama $ukuran_layar dan nilai 14.0
 $warna_laptop = "Silver"; // variabel dengan nama $warna_laptop dan nilai "Silver"
 
-echo "Merk Laptop\t: $merk_laptop \n";
-echo "Harga Laptop Rp\t: $harga_laptop \n";
-echo "Ukuran Layar\t: $ukuran_layar \n";
-echo "Warna Laptop\t: $warna_laptop \n";
+echo "Merk Laptop\t: $merk_laptop " . PHP_EOL;
+echo "Harga Laptop Rp\t: $harga_laptop" . PHP_EOL;
+echo "Ukuran Layar\t: $ukuran_layar " . PHP_EOL;
+echo "Warna Laptop\t: $warna_laptop " . PHP_EOL;
 
 var_dump($harga_laptop); // menampilkan tipe data dari variabel $harga_laptop
