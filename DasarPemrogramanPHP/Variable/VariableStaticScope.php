94e0279f2ef4b8a48636ff723a64f98dc4cb8d5f
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-06-25 17:49:47 +0700

    edit setting.json

---
+++ DasarPemrogramanPHP/Variable/VariableStaticScope.php
@@ -0,0 +1,14 @@
+<?php
+function increment () {
+    $counter = 0; 
+    echo "Counter: $counter".PHP_EOL;
+    $counter++;
+}
+increment(); // Counter: 0
+increment(); // Counter: 0  
+// Variabel $counter tidak menyimpan nilai dari pemanggilan sebelumnya
+
+function incrementStatic () {
+    static $counter = 0;
+    echo "Counter: $counter".PHP_EOL;
+}
