56ab8f67c36a200193f49856b1822796b5f62f20
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-07-01 21:53:01 +0700

    Menghubungkan setiap file pada folder menggunakan require_once pada file app.php

--- ProjectTodoList/app.php
+++ ProjectTodoList/app.php
@@ -1,5 +1,8 @@
 <?php
 
 require_once 'Model/TodoListApp.php';
+require_once 'BusinesLogic/viewTodoList.php';
+require_once 'BusinesLogic/addTodoList.php';
+require_once 'BusinesLogic/removeTodoList.php';
 
 echo "Aplikasi Todo List".PHP_EOL;
