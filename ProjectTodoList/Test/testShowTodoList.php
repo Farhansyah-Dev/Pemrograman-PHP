0efa5d3efffeb64cd2de850ddd5d8787175b687f
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-07-01 22:13:31 +0700

    Membuat Testing pada Aplikasi

---
+++ ProjectTodoList/Test/testShowTodoList.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../model/TodoListApp.php';
+require_once '../BusinesLogic/ShowTodoList.php';
+
+$todolist[1] = 'Belajar PHP Dasar' ;
+$todolist[2] = 'Belajar PHP OOP' ;
+$todolist[3] = 'Belajar PHP Web' ;
+
+
+ShowTodoList();
