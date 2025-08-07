0773217700ab4a9eb04a2369facc3aeab8f2fa74
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-08-07 16:41:57 +0700

    Testing function ShowTodoList

---
+++ ProjectTodoList/Test/TestShowTodoList.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../model/TodoListApp.php';
+require_once '../BusinesLogic/ShowTodoList.php';
+
+$todoList[1] = 'Belajar PHP Dasar' ;
+$todoList[2] = 'Belajar PHP OOP' ;
+$todoList[3] = 'Belajar PHP Web' ;
+
+
+showTodoList();
