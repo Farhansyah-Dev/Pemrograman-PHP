65955ee0ed337a4c1dcec7631ac195b518c8fa50
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-08-07 16:40:55 +0700

    Testing file input pada folder Helper

---
+++ ProjectTodoList/Test/TestInput.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "../Helper/input.php";
+
+$name = input("Nama");
+echo "hello $name" .PHP_EOL;
+
+$channel = input ("Nama channel");
+echo "saya sudah subsribe $channel" .PHP_EOL;
